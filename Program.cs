7c157c1f8ee28d1af63fc094fa3c27df98e7a76e
patrickpetty23 <pettyp@icloud.com>
2024-03-22 21:36:19 -0600

    Styling changes

--- Program.cs
+++ Program.cs
@@ -27,8 +27,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute("pagination", "Books/{pageNumber}", new {Controller = "Home", action = "Index"});
+app.MapDefaultControllerRoute();
 
 app.Run();
